c8e015324dd1724bb7f11af11cd6bbbda5bdd425
Jhanavi <jhanavibehl@gmail.com>
2024-08-01 15:09:08 -0400

    Client and Server

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+app.listen(3001, () => console.log("Server started"));
